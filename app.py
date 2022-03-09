7079de4ac16dc50f6d87f9812ca8cfed602043f7
burakuren101 <burakuren101@gmail.com>
2022-03-09 13:15:20 +0300

    extra print

--- app.py
+++ app.py
@@ -10,8 +10,6 @@ for current_dict in liked_songs_dict["tracks"]:
 
     id_list.append(current_dict["videoId"])
 
-playlist_ID = ytmusic.create_playlist(title="Myfavs",description="My Favs is a playlist now",privacy_status="PUBLIC",video_ids=id_list)
+playlist_ID = ytmusic.create_playlist(title="DENEME",description="My Favs is a playlist now",privacy_status="PUBLIC",video_ids=id_list)
 
 print(f"Successfully created the playlist here is id {playlist_ID}")
-
-print(id_list)
