1f0f10f0a1a58759e4519029215b8c89d2af5942
burakuren101 <burakuren101@gmail.com>
2022-03-09 13:39:32 +0300

    user is gonna give inputs from terminal

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from ytmusicapi import YTMusic
 
 ytmusic = YTMusic('headers_auth.json')
 
-liked_songs_dict = ytmusic.get_liked_songs(limit=800)
+
+print("I am gonna take your songs from liked list...")
+liked_songs_dict = ytmusic.get_liked_songs(limit=100000)
 
 id_list = []
 
@@ -10,6 +12,46 @@ for current_dict in liked_songs_dict["tracks"]:
 
     id_list.append(current_dict["videoId"])
 
-playlist_ID = ytmusic.create_playlist(title="DENEME",description="My Favs is a playlist now",privacy_status="PUBLIC",video_ids=id_list)
+title = input("Name of the new playlist: ")
+description = input("Add a description for your playlist: ")
 
-print(f"Successfully created the playlist here is id {playlist_ID}")
+
+cont = True
+while cont:
+
+    status = input("""
+    **************
+    
+    If you want to make your playlist PUBLIC type "pub"
+
+    If you want to make your playlist PRIVATE type "pri"
+
+    Type: """)
+    print("""
+    **************
+
+    """)
+
+    if status == "pub":
+        privacy_status = "PUBLIC"
+        cont = False
+
+    elif status == "pri":
+        privacy_status = "PRIVATE"
+        cont = False
+    
+    else:
+        print("It is invalid please try again!")
+        cont = True
+
+print("I am creating your new playlist please wait...")
+playlist_ID = ytmusic.create_playlist(title=title,description=description,privacy_status=privacy_status,video_ids=id_list)
+
+
+print(f"""
+--------------------------------------------------------------
+
+Successfully created the playlist! Here its id {playlist_ID}
+
+--------------------------------------------------------------
+""")
